a0e042b3d5f1800172ce6d1444085493357e45c4
aemann2 <aemann2@gmail.com>
2022-06-26 13:17:27 -0700

    added redirect logic for signed-in users

--- contact_keeper/views.py
+++ contact_keeper/views.py
@@ -19,11 +19,27 @@ class Profile(LoginRequiredMixin, TemplateView):
 class Login(LoginView):
     template_name = "registration/login.html"
 
+    # redirect a user to the home page if they're already logged in
+    def get(self, request, *args, **kwargs):
+        self.object = None
+        if request.user.is_anonymous:
+            return super().get(request, *args, **kwargs)
+        else:
+            return redirect("/accounts/profile/")
+
 
 class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+    # redirect a user to the home page if they're already signed up
+    def get(self, request, *args, **kwargs):
+        self.object = None
+        if request.user.is_anonymous:
+            return super().get(request, *args, **kwargs)
+        else:
+            return redirect("/accounts/profile/")
 
 
 def logout_view(request):
