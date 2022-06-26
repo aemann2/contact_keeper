ca01f25030897c83075b636e8b54be5c5d3549a4
aemann2 <aemann2@gmail.com>
2022-06-26 12:45:08 -0700

    added login

--- contact_keeper/views.py
+++ contact_keeper/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic.base import TemplateView
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
-class HomeView(TemplateView):
+class Home(TemplateView):
     template_name = "home.html"
+
+
+class Profile(TemplateView):
+    template_name = "accounts/profile.html"
+
+
+class Login(LoginView):
+    template_name = "registration/login.html"
