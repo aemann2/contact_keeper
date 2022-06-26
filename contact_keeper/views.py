f31c7d0a27ae7fa70880aa8aebd62a9c6ee01d46
aemann2 <aemann2@gmail.com>
2022-06-26 13:04:45 -0700

    added signup

--- contact_keeper/views.py
+++ contact_keeper/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.contrib.auth import logout
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.base import TemplateView
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
+from django.views.generic.base import TemplateView
+from django.views.generic import CreateView
 
 # Create your views here.
 class Home(TemplateView):
@@ -15,6 +18,12 @@ class Profile(LoginRequiredMixin, TemplateView):
 
 class Login(LoginView):
     template_name = "registration/login.html"
+
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
 
 
 def logout_view(request):
