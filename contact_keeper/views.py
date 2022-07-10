459ad0113cfbadc9b8b00db7e514e3b7e3ad9b12
aemann2 <aemann2@gmail.com>
2022-07-10 10:27:43 -0700

    added contact edit functionality

--- contact_keeper/views.py
+++ contact_keeper/views.py
@@ -27,12 +27,31 @@ def home(request):
     context = {"contact_form": form, "contacts": all_contacts}
     return render(request, "home.html", context)
 
+
 def delete_contact(request, pk):
     contact = get_object_or_404(Contact, pk=pk)
     if request.POST:
         contact.delete()
-        return redirect('/')
-    return render(request, 'home.html', {'contact': contact})
+        return redirect("/")
+    return render(request, "home.html", {"contact": contact})
+
+
+def edit_contact(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.POST:
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            contact.save()
+            form = ContactForm()
+            return redirect("/")
+    else:
+        # using instance property to populate form w/ retrieved data from Contact model
+        form = ContactForm(instance=contact)
+    all_contacts = Contact.objects.filter(owner=request.user)
+    # pk added to context so we can access it in the template
+    context = {"contact_form": form, "contacts": all_contacts, "pk": pk}
+    return render(request, "home.html", context)
+
 
 class Profile(LoginRequiredMixin, TemplateView):
     template_name = "home.html"
--- src/urls.py
+++ src/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from contact_keeper.views import Login, SignUp, home, delete_contact, logout_view
+from contact_keeper.views import Login, SignUp, home, delete_contact, edit_contact, logout_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -8,7 +8,8 @@ urlpatterns = [
     path("registration/login/", Login.as_view(), name="login"),
     path("registration/signup/", SignUp.as_view(), name="signup"),
     path("logout", logout_view, name="logout"),
-    path("delete/<int:pk>", delete_contact, name='delete_contact')
+    path("delete/<int:pk>", delete_contact, name='delete_contact'),
+    path("edit/<int:pk>", edit_contact, name='edit_contact')
 ]
 
 """src URL Configuration
