623647c5e4429a6769b17e7c00f4327329d0aff4
aemann2 <aemann2@gmail.com>
2022-07-10 15:03:12 -0700

    set placeholder values for form

--- contact_keeper/forms.py
+++ contact_keeper/forms.py
@@ -6,7 +6,16 @@ class ContactForm(ModelForm):
     class Meta:
         model = Contact
         fields = ["name", "email", "phone", "contact_type"]
-        widgets = {"contact_type": RadioSelect}
+        widgets = {
+            "contact_type": RadioSelect,
+        }
+
+    # overwrite __init__ to set placeholder values
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.fields["name"].widget.attrs["placeholder"] = "John Doe"
+        self.fields["email"].widget.attrs["placeholder"] = "john@gmail.com"
+        self.fields["phone"].widget.attrs["placeholder"] = "(415)555-0938"
 
     # commit named arg allows save to be delayed as we overwrite
     def save(self, commit=True):
