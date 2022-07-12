b4f2498a2691201a64560fe8cfa94634ca51afa5
aemann2 <aemann2@gmail.com>
2022-07-11 22:14:34 -0700

    began setting up tests

--- contact_keeper/tests.py
+++ contact_keeper/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import User
 
 # Create your tests here.
+class UserTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+            username="jdoe", email="john@gmail.com", password="12345678"
+        )
+        User.objects.create(
+            username="testUser", email="test@test.com", password="87654321"
+        )
+
+    def test_user_registered(self):
+        users = User.objects.all()
+        john = User.objects.get(username="jdoe")
+        self.assertEqual(users.count(), 2)
+        self.assertEqual(john.username, "jdoe")
