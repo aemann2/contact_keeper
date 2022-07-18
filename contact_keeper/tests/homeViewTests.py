aae1a5e185b0e127a056b1cf7754dd1bc5018c5c
aemann2 <aemann2@gmail.com>
2022-07-17 18:46:32 -0700

    wrote view tests...WIP

--- contact_keeper/tests/homeViewTests.py
+++ contact_keeper/tests/homeViewTests.py
@@ -1,14 +1,43 @@
 from django.test import TestCase
-from ..models import User
+from ..models import User, Contact
 
-class LogInPageTests(TestCase):
+
+class HomeViewTests(TestCase):
     def setUp(self):
         self.credentials = {"username": "jdoe", "password": "12345678"}
-        User.objects.create_user(**self.credentials)
+        self.john = User.objects.create_user(**self.credentials)
         # log in user
-        self.client.post(
-            "/registration/login/", self.credentials, follow=True
+        self.client.post("/registration/login/", self.credentials, follow=True)
+        Contact.objects.create(
+            owner=self.john,
+            name="Jim Doe",
+            email="jim@test.com",
+            phone="123-123-1233",
+            contact_type="Professional",
         )
-    
+
+    def test_form_post(self):
+        self.contact_info = {
+            "name": "Jane Doe",
+            "email": "jane@gmail.com",
+            "phone_0": "415-394-3948",
+            "contact_type": "Personal",
+        }
+        self.client.post("/", self.contact_info)
+        jane = Contact.objects.filter(name="Jane Doe").count()
+        self.assertEqual(jane, 1)
+
     def test_filter(self):
-        response = self.client.get("?filter=test")
+        response = self.client.get("?filter=jim")
+        contacts = response.context["contacts"].count()
+        self.assertEqual(contacts, 1)
+
+    def test_delete(self):
+        jim = Contact.objects.filter(name="Jim Doe").first()
+        response = self.client.post(f"/delete/{jim.pk}")
+        self.assertTemplateUsed(response, template_name="home.html")
+
+    def test_edit(self):
+        jim = Contact.objects.filter(name="Jim Doe").first()
+        response = self.client.post(f"/edit/{jim.pk}")
+        self.assertTemplateUsed(response, template_name="home.html")
