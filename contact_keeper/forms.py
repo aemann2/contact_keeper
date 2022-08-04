36e2e9f39099e66ab52a39346ea1ccba21c393ed
aemann2 <aemann2@gmail.com>
2022-08-04 10:33:08 -0700

    mobile styling

--- contact_keeper/forms.py
+++ contact_keeper/forms.py
@@ -1,6 +1,15 @@
-from django.forms import ModelForm, RadioSelect
+from django.forms import ModelForm, RadioSelect, CharField
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import AuthenticationForm
+from django.forms.widgets import PasswordInput, TextInput
 from .models import Contact
+
+
+class CustomAuthForm(AuthenticationForm):
+    username = CharField(
+        widget=TextInput(attrs={"class": "validate", "placeholder": "Username"})
+    )
+    password = CharField(widget=PasswordInput(attrs={"placeholder": "Password"}))
 
 
 class ContactForm(ModelForm):
@@ -18,9 +27,6 @@ class ContactForm(ModelForm):
             # popping 'pk' to avoid kwargs error
             kwargs.pop("pk")
         super(ContactForm, self).__init__(*args, **kwargs)
-        self.fields["name"].widget.attrs["placeholder"] = "John Doe"
-        self.fields["email"].widget.attrs["placeholder"] = "john@gmail.com"
-        self.fields["phone"].widget.attrs["placeholder"] = "(415)555-0938"
 
     # overwriting to check for duplicate entries
     def clean(self):
